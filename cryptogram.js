c954913e7afc2545c55c9af6e02eade88dab7340
Michael Watts <junkmailsink@gmail.com>
2021-04-03 02:13:45 -0700

    Tune styling; add comments

--- cryptogram.js
+++ cryptogram.js
@@ -7,6 +7,8 @@ cg = {
     freq_count: new Map(),
     mapping: new Map(),
 
+    // Re-layout everything. This gets called to refresh
+    // the page, anytime anything happens.
     handle_ciphertext: function() {
         // process for character frequency
         let ciphertext =
@@ -16,11 +18,14 @@ cg = {
         // display sorted list of characters
         this.show_mapping();
         // apply character mapping, if present
-        let pt_lines = this.apply_mapping(ct_lines);
         // display plaintext
-        this.show_plaintext(pt_lines);
+        this.show_plaintext(ct_lines);
     },
 
+    // For every character in the ciphertext, determine
+    // how many times it occurs. The results are stored
+    // in cg.freq_count . Counting is affected by the
+    // `case_insensitive` and `alpha_only` options.
     count_frequencies: function(lines) {
         this.freq_count.clear();
         this.freq_count.total_chars = 0;
@@ -39,6 +44,9 @@ cg = {
         }
     },
 
+    // Lay out the table of character frequencies in the center pane.
+    // This also lays out the controls which allow updating the
+    // decryption key, a map from ciphertext glyph to plaintext glyph.
     show_mapping: function() {
         let chars = Array.from(this.freq_count.keys());
         chars.sort( (a,b) => this.freq_count.get(b) - this.freq_count.get(a) );
@@ -61,8 +69,12 @@ cg = {
             }
             pt_glyph.onfocus = ( (e) => e.target.select() );
             pt_glyph.oninput = ( () => {
-                pt_glyph.value = pt_glyph.value.substr(0,1);
-                this.mapping.set(chr, pt_glyph.value);
+                if(pt_glyph.value) {
+                    pt_glyph.value = pt_glyph.value[0];
+                    this.mapping.set(chr, pt_glyph.value);
+                } else {
+                    this.mapping.delete(chr);
+                }
                 this.handle_ciphertext();
             } );
 
@@ -74,6 +86,10 @@ cg = {
         old_map_panel.replaceWith(map_panel);
     },
 
+    // Given an array of ciphertext lines, return an array of lines
+    // transformed by the decryption key.
+    // This is currently unused, since it made it difficult to render
+    // ciphertext and plaintext in different colors in the result.
     apply_mapping: function(ct_lines) {
         let pt_lines = [];
         for(const ct_line of ct_lines) {
@@ -108,6 +124,9 @@ cg = {
         return pt_lines;
     },
 
+    // Given an array of ciphertext lines, assemble a DOM structure
+    // to display the decrypted plaintext. Characters for which the
+    // decryption is not known appear, but in a faded color.
     show_plaintext: function(lines) {
         let span = document.createElement('span');
         let para = document.createElement('p');
@@ -124,15 +143,63 @@ cg = {
             if( add_line_break ) {
                 para.appendChild( document.createElement('br') );
             }
-            para.appendChild( document.createTextNode(line) );
+
+            let chunk = "";
+            let plaintext_mode = false;
+            let color_span = 0;
+            for(const chr of line) {
+                let normalized_chr = chr;
+                if(this.case_insensitive)
+                    { normalized_chr = chr.toUpperCase(); }
+                if( this.alpha_only && /[^a-z]/i.test(chr) ) {
+                    // If there is a leftover mapping for a
+                    // non-alphabetic character, ignore it.
+                    this.mapping.delete(chr);
+                }
+
+                if( !this.mapping.has(normalized_chr) ) {
+                    if(plaintext_mode) {
+                        color_span.appendChild(document.createTextNode(chunk));
+                        para.appendChild(color_span);
+                        chunk = chr;
+                        plaintext_mode = false;
+                    } else {
+                        chunk += chr;
+                    }
+                } else { // this.mapping.has(normalized_chr)
+                    let mapped_chr = this.mapping.get(normalized_chr);
+                    if(this.case_insensitive) {
+                        if( /[a-z]/.test(chr) )
+                            { mapped_chr = mapped_chr.toLowerCase(); }
+                        if( /[A-Z]/.test(chr) )
+                            { mapped_chr = mapped_chr.toUpperCase(); }
+                    }
+
+                    if(plaintext_mode) {
+                        chunk += mapped_chr;
+                    } else {
+                        para.appendChild( document.createTextNode(chunk) );
+                        chunk = mapped_chr;
+                        plaintext_mode = true;
+                        color_span = document.createElement('span');
+                        color_span.setAttribute('class','cg_deciphered');
+                    }
+                }
+            } // loop doesn't add the final chunk
+            if(plaintext_mode) {
+                color_span.appendChild(document.createTextNode(chunk));
+                para.appendChild(color_span);
+            } else {
+                para.appendChild( document.createTextNode(chunk) );
+            }
             add_line_break = true;
-        }
-        // that loop doesn't add the last paragraph
+        } // loop doesn't add the final paragraph
         span.appendChild(para);
 
         document.querySelector('#cg_plaintext > span').replaceWith(span);
     },
 
+    // Handle the "alphabetic only" checkbox
     toggle_alpha_only: function() {
         let alpha_only_checkbox =
             document.getElementById('cg_alpha_only');
@@ -140,6 +207,7 @@ cg = {
         this.handle_ciphertext();
     },
 
+    // Handle the "case-insensitive" checkbox
     toggle_case_insensitive: function() {
         let case_insensitive_checkbox =
             document.getElementById('cg_case_insensitive');
@@ -147,11 +215,14 @@ cg = {
         this.handle_ciphertext();
     },
 
+    // Handle the reset button. This wipes the decryption key.
     reset_key: function() {
         this.mapping.clear();
         this.handle_ciphertext();
     },
 
+    // Handle the ROT13 button. This sets the decryption key
+    // to a ROT13 map.
     rot13: function() {
         this.mapping.clear();
         let a = 'a'.charCodeAt(0);
